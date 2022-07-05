24a8c633042a3c453f72ae198e94e71f04b203f1
i-bilge <iismailbilge@gmail.com>
2022-07-05 10:29:26 +0200

    Menu and Navbar Componente added.

--- Week01/react_first/src/App.js
+++ Week01/react_first/src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Navbar from './components/Navbar';
+// import {DISHES} from './shared/dishes';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Menu/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-class App extends Component {
-   constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Menu/>
+    </div>
+  );
 }
-<Menu dishes={this.state.dishes} />
 
 export default App;
+
+// class App extends Component {
+//    constructor(props) {
+//     super(props);
+//     this.state = {
+//       dishes: DISHES
+//     };
+//   }
+// }
+// <Menu dishes={this.state.dishes} />
+
+// export default App;
