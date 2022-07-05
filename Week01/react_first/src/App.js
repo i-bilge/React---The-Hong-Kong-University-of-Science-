e3e7792a5cc8ea4413fb4e6573accb5b424f8624
i-bilge <iismailbilge@gmail.com>
2022-07-05 12:58:25 +0200

    state, prop and onClick adding studied.

--- Week01/react_first/src/App.js
+++ Week01/react_first/src/App.js
@@ -1,28 +1,40 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Menu from './components/MenuComponent';
 import Navbar from './components/Navbar';
-// import {DISHES} from './shared/dishes';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <Menu/>
-    </div>
-  );
-}
-
-export default App;
-
-// class App extends Component {
-//    constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: DISHES
-//     };
-//   }
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar/>
+//       <Menu/>
+//     </div>
+//   );
 // }
-// <Menu dishes={this.state.dishes} />
 
 // export default App;
+
+class App extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div className='row col-12'>
+          <Navbar/>
+        </div>
+        <div className='nav'>
+         <Menu dishes={this.state.dishes} />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
